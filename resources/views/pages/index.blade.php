973bc2f2888b77f5ed1c7362dc8c6126b73a4b2c
Zantezu <danilo.patrucco@gmail.com>
2019-07-06 15:12:38 -0400

    task : added jumbotron and created blade pages

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <h1>Hello World!</h1>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+@endsection
+
+@include('includes.jsfooter')
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return view('pages.index');
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
 });
